b2e27afcbace1af46162db8df4b747a19f738270
dimanne <dimanne@ya.ru>
2017-07-09 19:01:41 +0300

    trying to use gradients

--- tensorflow/cc/DimanNe/bitwise_operations/main.cpp
+++ tensorflow/cc/DimanNe/bitwise_operations/main.cpp
@@ -11,11 +11,14 @@ tf::Output AddLayer(tf::Scope                  s,
                     tfu::TVariableInitializer &InitV,
                     const tf::int64            NumOfInputs,
                     const tf::int64            LayerWidth,
-                    const tf::Output &         PrevOutput) {
+                    const tf::Output &         PrevOutput,
+                    tf::Output *               Weights) {
     using TRand = to::ParameterizedTruncatedNormal;
 
     // tf::Output b = InitV.Create<TRand>(s.WithOpName("b"), {1, LayerWidth}, tf::DT_DOUBLE, {}, 1., 1., 0., 3.);
     tf::Output W = InitV.Create<TRand>(s.WithOpName("W"), {NumOfInputs, LayerWidth}, tf::DT_DOUBLE, {}, 1., 2., -2., 3.);
+    if(Weights)
+        *Weights = W;
 
     tf::Output xW = to::MatMul(s, PrevOutput, W);
     // tf::Output xWb   = to::Add(s, xW, b);
@@ -23,19 +26,31 @@ tf::Output AddLayer(tf::Scope                  s,
     return Layer;
 }
 
-tf::Output CreateMLP(tf::Scope &s, tfu::TVariableInitializer &InitV, const tf::Output &x) {
+// tf::Output CreateMLP(tf::Scope &s, tfu::TVariableInitializer &InitV, const tf::Output &x) {
+//     const tf::PartialTensorShape ShapeOfInput    = tfu::GetShapeOfOutput(x);
+//     const tf::int64              NumOfElemInItem = ShapeOfInput.dim_sizes().back();
+//
+//     tf::Output Result = x;
+//     Result            = AddLayer(s.WithOpName("Layer1"), InitV, NumOfElemInItem, 3, Result);
+//     Result            = AddLayer(s.WithOpName("Layer2"), InitV, 3, 1, Result);
+//
+//     return Result;
+// }
+
+tf::Output CreateMLP(tf::Scope &s, tfu::TVariableInitializer &InitV, const tf::Output &x, tf::Output &Weights) {
     const tf::PartialTensorShape ShapeOfInput    = tfu::GetShapeOfOutput(x);
     const tf::int64              NumOfElemInItem = ShapeOfInput.dim_sizes().back();
 
     tf::Output Result = x;
-    Result            = AddLayer(s.WithOpName("Layer1"), InitV, NumOfElemInItem, 3, Result);
-    Result            = AddLayer(s.WithOpName("Layer2"), InitV, 3, 1, Result);
+    Result            = AddLayer(s.WithOpName("Layer1"), InitV, NumOfElemInItem, 1, Result, &Weights);
 
     return Result;
 }
 
+
 tf::Output AddLossFunction(tf::Scope &s, const tf::Output &ActualValue, const tf::Input &ExpectedValue) {
-    tf::Output Result = to::SquaredDifference(s.WithOpName("SqDiff"), ActualValue, ExpectedValue);
+    // tf::Output Result = to::SquaredDifference(s.WithOpName("SqDiff"), ActualValue, ExpectedValue);
+    tf::Output Result = to::Square(s.WithOpName("SqDiff"), ActualValue);
     return Result;
 }
 
@@ -46,10 +61,16 @@ int main() {
 
     const tf::Output x     = to::Placeholder(s.WithOpName("xInput"), tf::DT_DOUBLE, to::Placeholder::Attrs().Shape({-1, 2}));
     const tf::Output Expec = to::Placeholder(s.WithOpName("ExpVal"), tf::DT_DOUBLE, to::Placeholder::Attrs().Shape({-1, 1}));
-    const tf::Output Model = CreateMLP(s, InitV, x);
-    const tf::Output Loss  = AddLossFunction(s, Model, Expec);
+    // const tf::Output Model = CreateMLP(s, InitV, x);
+    tf::Output Weights;
+    const tf::Output Model = CreateMLP(s, InitV, x, Weights);
+
+    const tf::Output Loss = AddLossFunction(s, Model, Expec);
 
 
+    tf::Output dLossdLoss = to::Const(s, {1.});
+    tf::OutputList dxs;
+    TF_CHECK_OK(tf::AddSymbolicGradients(s, {Loss}, {Weights}, {dLossdLoss}, &dxs));
 
     std::vector<tf::Tensor> Outputs;
     // clang-format off
@@ -60,9 +81,15 @@ int main() {
     // clang-format on
     tf::ClientSession Session(s);
     InitV(Session);
-    TF_CHECK_OK(Session.Run(Feed, {Model, Loss}, &Outputs));
+    // TF_CHECK_OK(Session.Run(Feed, {Model, Loss}, &Outputs));
+    // LOG(INFO) << "Prediction: " << Outputs[0].matrix<double>();
+    // LOG(INFO) << "Loss:       " << Outputs[1].matrix<double>();
+
+    TF_CHECK_OK(Session.Run(Feed, dxs, &Outputs));
     LOG(INFO) << "Prediction: " << Outputs[0].matrix<double>();
-    LOG(INFO) << "Loss:       " << Outputs[1].matrix<double>();
+    // LOG(INFO) << "Loss:       " << Outputs[1].matrix<double>();
+
+
 
     return 0;
 }
@@ -129,3 +156,9 @@ int main() {
 //  private:
 //   bool use_exclusive_lock_;
 // };
+
+
+
+
+// Compiler run:
+// /usr/lib/gcc/x86_64-linux-gnu/6/cc1plus -quiet -imultiarch x86_64-linux-gnu -MD bazel-out/local-opt/bin/tensorflow/cc/_objs/math_grad/tensorflow/cc/gradients/math_grad.d -MF bazel-out/local-opt/bin/tensorflow/cc/_objs/math_grad/tensorflow/cc/gradients/math_grad.d -MQ bazel-out/local-opt/bin/tensorflow/cc/_objs/math_grad/tensorflow/cc/gradients/math_grad.o -D_GNU_SOURCE -U _FORTIFY_SOURCE -D _FORTIFY_SOURCE=1 -D NDEBUG -D EIGEN_MPL2_ONLY -D TENSORFLOW_USE_JEMALLOC -D SNAPPY -D __DATE__="redacted" -D __TIMESTAMP__="redacted" -D __TIME__="redacted" -iquote . -iquote bazel-out/local-opt/genfiles -iquote external/jemalloc -iquote bazel-out/local-opt/genfiles/external/jemalloc -iquote external/bazel_tools -iquote bazel-out/local-opt/genfiles/external/bazel_tools -iquote external/protobuf -iquote bazel-out/local-opt/genfiles/external/protobuf -iquote external/eigen_archive -iquote bazel-out/local-opt/genfiles/external/eigen_archive -iquote external/local_config_sycl -iquote bazel-out/local-opt/genfiles/external/local_config_sycl -iquote external/gif_archive -iquote bazel-out/local-opt/genfiles/external/gif_archive -iquote external/jpeg -iquote bazel-out/local-opt/genfiles/external/jpeg -iquote external/com_googlesource_code_re2 -iquote bazel-out/local-opt/genfiles/external/com_googlesource_code_re2 -iquote external/farmhash_archive -iquote bazel-out/local-opt/genfiles/external/farmhash_archive -iquote external/fft2d -iquote bazel-out/local-opt/genfiles/external/fft2d -iquote external/highwayhash -iquote bazel-out/local-opt/genfiles/external/highwayhash -iquote external/png_archive -iquote bazel-out/local-opt/genfiles/external/png_archive -iquote external/zlib_archive -iquote bazel-out/local-opt/genfiles/external/zlib_archive -iquote external/snappy -iquote bazel-out/local-opt/genfiles/external/snappy -iquote external/local_config_cuda -iquote bazel-out/local-opt/genfiles/external/local_config_cuda -iquote external/curl -iquote bazel-out/local-opt/genfiles/external/curl -iquote external/boringssl -iquote bazel-out/local-opt/genfiles/external/boringssl -iquote external/jsoncpp_git -iquote bazel-out/local-opt/genfiles/external/jsoncpp_git -isystem external/jemalloc/include -isystem bazel-out/local-opt/genfiles/external/jemalloc/include -isystem external/bazel_tools/tools/cpp/gcc3 -isystem external/protobuf/src -isystem bazel-out/local-opt/genfiles/external/protobuf/src -isystem external/eigen_archive -isystem bazel-out/local-opt/genfiles/external/eigen_archive -isystem external/gif_archive/lib -isystem bazel-out/local-opt/genfiles/external/gif_archive/lib -isystem external/farmhash_archive/src -isystem bazel-out/local-opt/genfiles/external/farmhash_archive/src -isystem external/png_archive -isystem bazel-out/local-opt/genfiles/external/png_archive -isystem external/zlib_archive -isystem bazel-out/local-opt/genfiles/external/zlib_archive -isystem external/local_config_cuda/cuda -isystem bazel-out/local-opt/genfiles/external/local_config_cuda/cuda -isystem external/local_config_cuda/cuda/include -isystem bazel-out/local-opt/genfiles/external/local_config_cuda/cuda/include -isystem external/curl/include -isystem bazel-out/local-opt/genfiles/external/curl/include -isystem external/boringssl/src/include -isystem bazel-out/local-opt/genfiles/external/boringssl/src/include -isystem external/jsoncpp_git/include -isystem bazel-out/local-opt/genfiles/external/jsoncpp_git/include tensorflow/cc/gradients/math_grad.cc -quiet -dumpbase math_grad.cc -mavx -msse4.2 -mtune=generic -march=x86-64 -auxbase-strip bazel-out/local-opt/bin/tensorflow/cc/_objs/math_grad/tensorflow/cc/gradients/math_grad.o -g0 -g -O2 -Wall -Wunused-but-set-parameter -Wno-free-nonheap-object -Wno-builtin-macro-redefined -std=c++11 -fstack-protector -fno-omit-frame-pointer -ffunction-sections -fdata-sections -frandom-seed=bazel-out/local-opt/bin/tensorflow/cc/_objs/math_grad/tensorflow/cc/gradients/math_grad.o -fno-canonical-system-headers -Wformat-security -o /tmp/cctrFkMC.s
